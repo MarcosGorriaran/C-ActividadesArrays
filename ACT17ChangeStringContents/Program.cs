d51f75cab47d056e414c1e3283efb0317288e2b9
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2023-11-04 22:23:46 +0100

    Actividad 28 acabado

---
+++ ACT17ChangeStringContents/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Gorriaran Caamaño Marcos Facundo
+ * 4/11/2023
+ * Exercici 17
+ * Implementa un programa que demani a l’usuari que introdueixi un text i 
+ * el retorni tot en majúscules, tot en minúscules i la primera lletra de 
+ * cada paraula en majúscules (per separat).
+ */
+using System;
+
+namespace CSharpArrays
+{
+    class ActivityTwentySeven
+    {
+        public static void Main()
+        {
+            const string AskText = "Proporcioname un texto: ";
+            const char SpaceSearchTarget = ' ';
+
+            string userText;
+            char[] userTextChar;
+
+            Console.Write(AskText);
+            userText = Console.ReadLine();
+
+            Console.WriteLine(userText.ToUpper());
+            Console.WriteLine(userText.ToLower());
+            userTextChar = userText.ToCharArray();
+            userText = "";
+            for(int i = 0; i < userTextChar.Length-1; i++)
+            {
+                if (userTextChar[i].Equals(SpaceSearchTarget))
+                {
+                     userTextChar[i+1] = Convert.ToChar(Convert.ToString(userTextChar[i+1]).ToUpper());
+                }
+                userText += userTextChar[i];
+            }
+            userText += userTextChar[userTextChar.Length - 1];
+            Console.WriteLine(userText);
+        }
+    }
+}
---
+++ ACT18IsOddEvenWithStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Gorriaran Caamaño Marcos Facundo
+ * 4/11/2023
+ * Exercici 18
+ * Fent servir la interpolació i l’operador ternari, implementa un programa
+ * que indiqui pels 20 primers nombres (del 1 al 20 inclosos) si és parell
+ * o senar (només ha d’indicar per a cada valor “És parell”/”És senar)”.
+ */
+
+using System;
+
+namespace CSharpArrays
+{
+    class ActivityTwentyEight
+    {
+        public static void Main()
+        {
+            const int MinValue = 1;
+            const int MaxValue = 20;
+            const int Div = 2;
+            const int EvenResult = 0;
+            const string IsEvenOdd = "{0}: {1}";
+            const string Even = "Es par";
+            const string Odd = "Es impar";
+
+            for (int i = MinValue; i <= MaxValue; i++)
+            {
+                Console.WriteLine(IsEvenOdd,i,i%Div==EvenResult ? Even : Odd);
+            }
+        }
+    }
+}
