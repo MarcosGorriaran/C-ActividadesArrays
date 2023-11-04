952964e8c4030e16a8354c44d7d8a408628abb8e
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2023-11-04 21:37:05 +0100

    Acabado actividad 24

---
+++ ACT14Interpolacion/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Gorriaran Caamaño Marcos Facundo
+ * 4/11/2023
+ * Exercici 14
+ * Fent servir la interpolació, implementa un programa en el que es demani 
+ * a l’usuari el seu nom, cognoms i edat i retorni un missatge informatiu,
+ * com a l’exemple:
+ */
+using System;
+
+namespace CSharpArrays
+{
+    class ActivityTwentyFour
+    {
+        public static void Main()
+        {
+            const int AmountOfFillers = 3;
+            const int NamePosition = 0;
+            const int SurnamePosition = 1;
+            const int AgePosition = 2;
+            const string OutputMessege = "L'{0} {1} té {2} anys";
+            const string AskValue = "Proporcioname amb la paraula {0}: ";
+
+            string[] fillingWords = new string[AmountOfFillers];
+
+            for(int i = 0; i < AmountOfFillers; i++)
+            {
+                Console.Write(AskValue,i+1);
+                fillingWords[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine(OutputMessege, fillingWords[NamePosition], fillingWords[SurnamePosition], fillingWords[AgePosition]);
+        }
+    }
+}
---
+++ ACT15ToyingWithStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Gorriaran Caamaño Marcos Facundo
+ * 25/10/2023
+ * Exercici 15
+ * Implementa un programa que emmagatzemi una frase introduïda per teclat 
+ * en la variable textOne. Demana un altre text i emmagatzema’l en la 
+ * variable textTwo. Afegeix textTwo a textOne i mostra-la per pantalla.
+ * És correcte? Què pots observar a les variables textOne i textTwo?
+ */
+using System;
+
+namespace CSharpArrays
+{
+    class ActivityTwentyFive
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
