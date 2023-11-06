61af9a9763eb56f74f090e225978f8bed9dac498
MarcosGorriaran <Marcos.Gorriaran.7e7@itb.cat>
2023-11-06 14:00:33 +0100

    Proceso Actividad 30

---
+++ ACT20OnlyShowingFirstLetter/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Gorriaran Caamaño Marcos Facundo
+ * 4/11/2023
+ * Exercici 20
+ */
+using System;
+
+namespace CSharpArrays
+{
+    class ActivityThirty
+    {
+        public static void Main()
+        {
+            const char SplitFirstTarget = ',';
+            const string SplitSecondTarget = " i ";
+            const string AskText = "Proporcioname a todos los miembros del grupo";
+
+            string fullText;
+            string[] splitedText;
+            string[] splitedLastPortion;
+            string[] allSplited;
+
+            Console.WriteLine(AskText);
+            splitedText = Console.ReadLine().Split(SplitFirstTarget);
+            splitedLastPortion = splitedText[splitedText.Length - 1].Split(SplitSecondTarget);
+            allSplited = new string[splitedText.Length + splitedLastPortion.Length];
+
+            for (int i = 0; i < allSplited.Length; i++)
+            {
+                allSplited[i] = i < splitedText.Length ? splitedText[i] : splitedLastPortion[i - splitedText.Length];
+                Console.WriteLine(allSplited[i].Trim().ToUpper()[0]);
+            }
+        }
+    }
+}
